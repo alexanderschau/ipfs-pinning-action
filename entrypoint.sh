389c3c2ef8a5bbb76958b8f283808ec0787af8e8
Marcin Rataj <lidel@lidel.org>
2021-04-08 21:49:28 +0200

    feat: CIDv1
    CIDv1 will soon become the default, may be beneficial to use it sooner here
    so the text representation is compatible with subdomain gateways:
    https://docs.ipfs.io/how-to/address-ipfs-on-web/#subdomain-gateway
    and won't suddenly change in the future, when the default flips.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ ipfs init
 ipfs daemon &
 waitForIpfs
 ipfs pin remote service add origin $ENDPOINT $ACCESS_TOKEN
-CID=$(ipfs add -r -q $UPLOAD_PATH | tail -1)
+CID=$(ipfs add -r -q --cid-version 1 $UPLOAD_PATH | tail -1)
 ipfs pin remote add --service=origin --name=$NAME $CID
 
 echo "::set-output name=cid::$CID"
